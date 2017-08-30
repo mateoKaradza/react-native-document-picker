cb53a2af9b623924a14254d72f710e585db50f6d
Mateo Karadza <mateo@eastcoastproduct.com>
2017-08-30 13:16:28 +0200

    0.47

--- android/src/main/java/com/reactnativedocumentpicker/ReactNativeDocumentPicker.java
+++ android/src/main/java/com/reactnativedocumentpicker/ReactNativeDocumentPicker.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 public class ReactNativeDocumentPicker implements ReactPackage {
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
